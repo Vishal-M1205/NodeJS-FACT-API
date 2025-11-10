42010ad5c98af1bca4af31c1d990b58d33f23e39
Vishal-M1205 <vishalmanivannan1205@gmail.com>
2025-11-10 16:21:11 +0530

    Completed

--- server.js
+++ server.js
@@ -20,9 +20,40 @@ const server = http.createServer( async (req,res)=>{
     res.end(JSON.stringify(fact));
    }
 
- 
-   
-
+  if(req.method == "GET" && req.url == '/fact/dog'){
+    const data = await fs.readFile('Dog.json','utf-8');
+    const fact = JSON.parse(data)
+    res.writeHead(200,'Success');
+    res.end(JSON.stringify(fact));
+  }  
+  if(req.method == "GET" && req.url == '/fact/horse'){
+    const data = await fs.readFile('Horse.json','utf-8');
+    const fact = JSON.parse(data)
+    res.writeHead(200,'Success');
+    res.end(JSON.stringify(fact));
+  }
+if(req.method == "GET" && req.url == '/fact/tiger'){
+    const data = await fs.readFile('Tiger.json','utf-8');
+    const fact = JSON.parse(data)
+    res.writeHead(200,'Success');
+    res.end(JSON.stringify(fact));
+  }
+  if(req.method == 'POST' && req.url == '/fact/create'){
+  let body = "";
+  req.on('data',chunk => {
+    body += chunk.toString();
+  })
+  req.on('end',async ()=>{
+      const parsed = JSON.parse(body);
+      const{title,facts} = parsed;
+      await fs.writeFile(`${title}.json`,JSON.stringify(facts),'utf-8');
+      res.writeHead(201);
+      res.end(JSON.stringify({
+        message  : "Data Created",
+        data : parsed
+      }))
+  })
+  }
 })
 
 server.listen(8000);
